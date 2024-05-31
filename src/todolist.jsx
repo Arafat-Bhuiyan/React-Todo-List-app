4ffdb92870ddd178c794093898d110afb2fe4107
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2024-05-31 19:40:25 +0600

    functions added

---
+++ src/todolist.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+function TodoList(){
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState("");
+
+    function handleInputChange(event){
+
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+
+    function moveTaskDown(index){
+
+    }
+
+    return (
+        <div></div>
+    )
+}
+
+export default TodoList
